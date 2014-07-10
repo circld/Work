ca54aa93c9196ef77364ff53168f2a132aa4cfd9
circld <circld@gmail.com>
2014-07-10 19:58:24 -0400

    Added stemming/removing noisy words

--- manager_match.py
+++ manager_match.py
@@ -3,32 +3,13 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import re
 
-# TO DO:
-# incorporate remove_noisy_words into script & rerun fuzzy matching
-
-# import data
-ff_raw = pd.read_excel('./Desktop/managers.xlsx', 'FF')
-si_raw = pd.read_excel('./Desktop/managers.xlsx', 'SI')
-si_total_raw = pd.read_excel('./Desktop/managers.xlsx', 'si_total')
-
-# drop NA values
-si = si_raw.dropna()
-ff = ff_raw.dropna()
-si_total = si_total_raw.dropna()
-
-# clean and standardize formatting using fuzz module
-ff['ManagerName'] = ff['ManagerName'].map(fuzz.full_process)
-si['ManagerName'] = si['ManagerName'].map(fuzz.full_process)
-si_total['ManagerName'] = si_total['ManagerName'].map(fuzz.full_process)
-
-# remove common words with little signalling value
-
-
-# create lists
-si_list = [i for i in si['ManagerName']]
-ff_list = [i for i in ff['ManagerName']]
-si_total_list = [i for i in si_total['ManagerName']]
-
+# words to strip out
+noisy_words = ['invest', 'asset', 'manager', 'partners', 'limited', 'capital'
+                , 'corp', 'associate', 'research', 'financial', 'bank', 'management'
+                , 'fund', 'group', 'plc', 'ltd', 'company', 'holding', 'service'
+                , 'portfolio', 'banco', 'gruppen'
+              ]
+# helper functions
 def match_lists(list1, list2, precision=90):
     '''
     Takes two lists
@@ -47,23 +28,51 @@ def match_lists(list1, list2, precision=90):
     return [in_both, not_in_both]
 
 def get_match(word, word_list, precision=90):
+    if len(word) == 0:
+        return ''
     best_match = process.extractBests(word, word_list, limit=1)
     if best_match[0][1] <= precision:
         best_match = ''
     return best_match
 
-noisy_words = ['invest', 'asset', 'manage', 'partners', 'limited', 'capital'
-                , 'corp', 'associate', 'research', 'financ']
 def remove_noisy_words(word):
     for stem in noisy_words:
-        pattern = re.compile('\w*' + stem + '\w*', flags=re.IGNORECASE)
+        pattern = re.compile(stem + 's?', flags=re.IGNORECASE)
         word = re.sub(pattern, '', word)
     return word
 
+# import data
+ff_raw = pd.read_excel('./Desktop/managers.xlsx', 'FF')
+si_raw = pd.read_excel('./Desktop/managers.xlsx', 'SI')
+si_total_raw = pd.read_excel('./Desktop/managers.xlsx', 'si_total')
+
+# drop NA values
+si = si_raw.dropna()
+ff = ff_raw.dropna()
+si_total = si_total_raw.dropna()
+
+# clean and standardize formatting using fuzz module
+ff['CleanManagerName'] = ff['ManagerName'].map(fuzz.full_process)
+si['CleanManagerName'] = si['ManagerName'].map(fuzz.full_process)
+si_total['CleanManagerName'] = si_total['ManagerName'].map(fuzz.full_process)
+
+# remove common words with little signalling value
+ff['ManagerStem'] = ff['ManagerName'].map(remove_noisy_words)
+si['ManagerStem'] = si['ManagerName'].map(remove_noisy_words)
+si_total['ManagerStem'] = si_total['ManagerName'].map(remove_noisy_words)
+
+# create lists
+si_list = [i for i in si['ManagerStem']]
+ff_list = [i for i in ff['ManagerStem']]
+si_total_list = [i for i in si_total['ManagerStem']]
+
+
 if __name__ == '__main__':
-    ff['ManagerSI'] = ff.ManagerName.map(lambda x : get_match(x, si_list, 80))
+    # creating column with best match (according to precision)
+    ff['ManagerSI'] = ff.ManagerStem.map(lambda x : get_match(x, si_list, 80))
+    # unmatched FF CB managers
     ff_only = ff[ff['ManagerSI'] == '']
-    ff_only['LocalManagerSI'] = ff_only.ManagerName.map(lambda x : get_match(x, si_total_list, 80))
+    ff_only['LocalManagerSI'] = ff_only.ManagerStem.map(lambda x : get_match(x, si_total_list, 80))
     ff_only.to_csv('Desktop/ff_only.csv', encoding='utf-8')
     #ff_both, ff_only = match_lists(ff_list, si_list, 80)
     #si_both, si_only = match_lists(si_list, ff_list)
